33c95787c11d179b0b715f75f0499667f331b879
superbrothers <superbrothers@shisobu.in>
2012-05-28 22:55:35 +0900

    imageviewurlreplace を関数化

--- README.md
+++ README.md
@@ -11,13 +11,15 @@ $ npm install imageviewurlreplace
 ```javascript
 var ivur = require("imageviewurlreplace");
 
-ivur.timeout = 1000; // http request timeout (default: 1000 ms)
-
 ivur.on("error", function (error, res, body) {
     console.log(error);
 });
 
-ivur.fetch("http://ascii.jp/elem/000/000/686/686849/img.html", function (url, res, referer) {
+ivur({uri: "http://ascii.jp/elem/000/000/686/686849/img.html"}, function (url, referer, res) {
+    console.log(url); // http://ascii.jp/elem/000/000/686/686849/sv1_c_640x480.jpg
+});
+
+ivur("http://ascii.jp/elem/000/000/686/686849/img.html", function (url, referer, res) {
     console.log(url); // http://ascii.jp/elem/000/000/686/686849/sv1_c_640x480.jpg
 });
 ```
--- test/ImageViewURLReplaceTest.js
+++ test/ImageViewURLReplaceTest.js
@@ -4,19 +4,19 @@ var path = require("path")
 module.exports = {
     "EXTRACT Mode": {
       "単一の$EXTRACT": function (test) {
-      ivur.fetch("http://ascii.jp/elem/000/000/686/686849/img.html", function (url, referer, res) {
+      ivur("http://ascii.jp/elem/000/000/686/686849/img.html", function (url, referer, res) {
         test.equal(url, "http://ascii.jp/elem/000/000/686/686849/sv1_c_640x480.jpg");
         test.done();
       });
     }
     , "複数の$EXTRACT": function (test) {
-      ivur.fetch("http://www.vector.co.jp/magazine/softnews/120414/n1204141.html", function (url, referer, res) {
+      ivur("http://www.vector.co.jp/magazine/softnews/120414/n1204141.html", function (url, referer, res) {
         test.equal(url, "http://www.vector.co.jp/magazine/softnews/120414/images/n12041411b.gif");
         test.done();
       });
     }
     , "リファラ付き": function (test) {
-      ivur.fetch("http://hobby-c.rash.jp/igafioz/ref/1330986364242.htm", function (url, referer, res) {
+      ivur("http://hobby-c.rash.jp/igafioz/ref/1330986364242.htm", function (url, referer, res) {
         test.equal(url, "http://hobby-c.rash.jp/igafioz/src/1330986364242.jpg");
         test.done();
       });
@@ -24,7 +24,7 @@ module.exports = {
   }
   , "VIEWER Mode": {
     "正しく取得できる": function (test) {
-      ivur.fetch("http://map.yahoo.co.jp/pl?type=scroll&lat=35.67832667&lon=139.77044378&sc=3&mode=map&pointer=on", function (url, referer, res) {
+      ivur("http://map.yahoo.co.jp/pl?type=scroll&lat=35.67832667&lon=139.77044378&sc=3&mode=map&pointer=on", function (url, referer, res) {
         test.equal(url, "http://img.map.yahoo.co.jp/ymap/mk_map?type=scroll&lat=35.67832667&lon=139.77044378&sc=3&mode=map&pointer=on");
         test.done();
       });
@@ -32,7 +32,7 @@ module.exports = {
   }
   , "DEFAULT Mode": {
     "正しく取得できる": function (test) {
-      ivur.fetch("http://upload.wikimedia.org/wikipedia/en/f/fa/Google_Chrome_2011_computer_icon.svg", function (url, referer, res) {
+      ivur("http://upload.wikimedia.org/wikipedia/en/f/fa/Google_Chrome_2011_computer_icon.svg", function (url, referer, res) {
         test.equal(url, "http://upload.wikimedia.org/wikipedia/en/thumb/f/fa/Google_Chrome_2011_computer_icon.svg/1000px-Google_Chrome_2011_computer_icon.svg.png");
         test.done();
       });
@@ -40,7 +40,7 @@ module.exports = {
   }
   , "NG": {
     "正しくNG判定できている": function (test) {
-      ivur.fetch("http://ipatukouta.altervista.org/test.jpg", function (url, referer, res) {
+      ivur("http://ipatukouta.altervista.org/test.jpg", function (url, referer, res) {
         test.equal(url, null);
         test.done();
       });
@@ -48,12 +48,11 @@ module.exports = {
   }
   , "Error": {
     "http request timeout": function (test) {
-      ivur.timeout = 1;
       ivur.on("error", function (error) {
         test.equal(error.code, "ETIMEDOUT");
         test.done();
       });
-      ivur.fetch("http://ascii.jp/elem/000/000/686/686849/img.html", function () {});
+      ivur({uri: "http://ascii.jp/elem/000/000/686/686849/img.html", timeout: 1}, function () {});
     }
   }
 };
