a68bb882a71ddb00e939dac86334bb2550e26038
Kazuki Suda <kazuki.suda@gmail.com>
2013-01-06 16:31:55 +0900

    Pass JSLint

--- index.js
+++ index.js
@@ -1,3 +1,3 @@
+/*jslint node: true, nomen: true, maxerr: 50, indent: 4 */
 var path = require("path");
 module.exports = require(path.join(__dirname, "lib", "imageviewurlreplace.js"));
-// vim: set fenc=utf-8 ts=2 sts=2 sw=2
